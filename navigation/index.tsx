77c8833607c4fa55e21f30f5076ad67cac89c5f1
Firmansyah Yanuar <firmansyah.yanuar@autobahn-security.com>
2022-06-13 01:57:10 +0700

    refactor: remove `edit screen info`

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -66,7 +66,7 @@ function BottomTabNavigator() {
         name="TabOne"
         component={TabOneScreen}
         options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
+          title: 'Transaction',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Pressable
